e7fb8cd61b03332bfb9195aee995d485c36777b7
thalun <tharungowda0046@gmail.com>
2025-06-26 20:58:51 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+name="michail"
+department="Nuclear Science"
+print("he was a russian named",name,"working in",department,"during communism")
+print("he had success in his early science career in chemical engineering")
+atomictrials=12
+hydrogentrials=34
+print("he tested",atomictrials,"atomic trials","and",hydrogentrials,"hydrogen trials" )
+total=float(atomictrials+hydrogentrials)
+print("that's a total of",total,"trials he could achieve before his mysterious death")
